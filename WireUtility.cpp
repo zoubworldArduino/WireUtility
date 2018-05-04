78d802a9e2e2a302d042b513055c7ea3789ba933
Pierre Valleau <zoubworld_arduino@yahoo.com>
2018-05-04 22:51:24 +0200

    change global variable name

--- WireUtility.cpp
+++ WireUtility.cpp
@@ -13,30 +13,32 @@
 // Version 4, March 3, 2013, Using Arduino 1.0.3
 //    by Arduino.cc user Krodal.
 //    Changes by louarnold removed.
-//    Scanning addresses changed from 0...127 to 1...119,
+//    Scanning WireUtility_addresses changed from 0...127 to 1...119,
 //    according to the i2c scanner by Nick Gammon
 //    http://www.gammon.com.au/forum/?id=10896
 // Version 5, March 28, 2013
-//    As version 4, but address scans now to 127.
-//    A sensor seems to use address 120.
+//    As version 4, but WireUtility_address scans now to 127.
+//    A sensor seems to use WireUtility_address 120.
 // 
 // version 6, based on previous job create library WireUtility
 // (c) zoubata
-// This sketch tests the standard 7-bit addresses
-// Devices with higher bit address might not be seen properly.
+// This sketch tests the standard 7-bit WireUtility_addresses
+// Devices with higher bit WireUtility_address might not be seen properly.
 //
-//https://www.i2c-bus.org/addressing/
+//https://www.i2c-bus.org/WireUtility_addressing/
 
 #include <Wire.h>
 #include <WireUtility.h>
 
 
 
-int  address;
+int  WireUtility_address;
 int scan(HardwareSerial &MySerial,TwoWire &ScanWire)
 {
+ // ScanWire.begin(123);
   ScanWire.begin();
-  address = 1;
+  
+  WireUtility_address = 1;
   return scanNext(MySerial,ScanWire);
 }
 //1111 0XX
@@ -47,30 +49,30 @@ int scanNext10bits(HardwareSerial &MySerial,TwoWire &ScanWire)
 
   MySerial.println("Scanning...");
 
-   for(; address <= 0x7BFF; address++ ) 
+   for(; WireUtility_address <= 0x7BFF; WireUtility_address++ ) 
   {
     // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    ScanWire.beginTransmission(address>>7);
-	ScanWire.write(address&0x7f);
+    // a device did acknowledge to the WireUtility_address.
+    ScanWire.beginTransmission(WireUtility_address>>7);
+	ScanWire.write(WireUtility_address&0x7f);
     error = ScanWire.endTransmission();
 
     if (error == 0)
     {
-      MySerial.print("I2C device found at address 0x");
-      MySerial.print(address,HEX);
+      MySerial.print("I2C device found at WireUtility_address 0x");
+      MySerial.print(WireUtility_address,HEX);
       MySerial.println("  !");
 
-	address++;
-	return address-1;    
+	WireUtility_address++;
+	return WireUtility_address-1;    
     }
     else if (error==4) 
     {
-      MySerial.print("Unknow error at address 0x");
-      if (address<16) 
+      MySerial.print("Unknow error at WireUtility_address 0x");
+      if (WireUtility_address<16) 
         MySerial.print("0");
-      MySerial.println(address,HEX);
+      MySerial.println(WireUtility_address,HEX);
     }    
   }
   return 0;
@@ -80,46 +82,46 @@ int scanNext(HardwareSerial &MySerial,TwoWire &ScanWire)
   byte error;
   int nDevices;
 
-if (address>=0x7800)
+if (WireUtility_address>=0x7800)
  return scanNext10bits(MySerial,ScanWire);
   nDevices = 0;
-  for(; address < 127; address++ ) 
+  for(; WireUtility_address < 127; WireUtility_address++ ) 
   {
     // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    ScanWire.beginTransmission(address);
+    // a device did acknowledge to the WireUtility_address.
+    ScanWire.beginTransmission(WireUtility_address);
     error = ScanWire.endTransmission();
 
     if (error == 0)
     {
-      MySerial.print("I2C device found at address 0x");
-      if (address<16) 
+      MySerial.print("I2C device found at WireUtility_address 0x");
+      if (WireUtility_address<16) 
         MySerial.print("0");
-      MySerial.print(address,HEX);
+      MySerial.print(WireUtility_address,HEX);
       MySerial.println("  !");
 
-	  if (address==0x0)  	 MySerial.println(" 0000000 0 / 0000000 1 General Call(0) or a Start Byte(1)");
- if (address==0x1)  	 MySerial.println(" 0000001 X CBUS Addresses");
- if (address==0x2)  	 MySerial.println(" 0000010 X Reserved for Different Bus Formats");
- if (address==0x3)  	 MySerial.println(" 0000011 X Reserved for future purposes");
- if (address&0xFC==0x4)  	 MySerial.println(" 00001XX X High-Speed Master Code");
- if (address&0xFC==0x78)  	 {
+	  if (WireUtility_address==0x0)  	 MySerial.println(" 0000000 0 / 0000000 1 General Call(0) or a Start Byte(1)");
+ if (WireUtility_address==0x1)  	 MySerial.println(" 0000001 X CBUS Addresses");
+ if (WireUtility_address==0x2)  	 MySerial.println(" 0000010 X Reserved for Different Bus Formats");
+ if (WireUtility_address==0x3)  	 MySerial.println(" 0000011 X Reserved for future purposes");
+ if (WireUtility_address&0xFC==0x4)  	 MySerial.println(" 00001XX X High-Speed Master Code");
+ if (WireUtility_address&0xFC==0x78)  	 {
 		MySerial.println("11110XX X 10-bit Slave Addressing :  11110XX X xxxxxxxx");
-			address=0x7800;
+			WireUtility_address=0x7800;
 		return scanNext10bits(MySerial,ScanWire);
 	}
- if (address&0xFC==0x7C)  	 MySerial.println("11111XX X Reserved for future purposes");
+ if (WireUtility_address&0xFC==0x7C)  	 MySerial.println("11111XX X Reserved for future purposes");
  
-    address++;
-	return address-1;     
+    WireUtility_address++;
+	return WireUtility_address-1;     
     }
     else if (error==4) 
     {
-      MySerial.print("Unknow error at address 0x");
-      if (address<16) 
+      MySerial.print("Unknow error at WireUtility_address 0x");
+      if (WireUtility_address<16) 
         MySerial.print("0");
-      MySerial.println(address,HEX);
+      MySerial.println(WireUtility_address,HEX);
     }    
   }
   return 0;
